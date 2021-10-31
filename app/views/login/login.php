283cd133ae6477c73306a8f0e36486a860d9181e
Dricko29 <dricko0798@gmail.com>
2021-10-31 18:14:29 +0700

    .

--- app/views/login/login.php
+++ app/views/login/login.php
@@ -20,58 +20,56 @@ href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
 rel="stylesheet">
 </head>
 <body class="hold-transition login-page">
-<div class="login-box">
- <div class="login-logo">
- <b>Bu</b>Kita
- </div>
- <!-- /.login-logo -->
- <div class="card">
- <div class="card-body login-card-body">
- <p class="login-box-msg">Silahkan login terlebih dahulu.</p>
- <form action="<?= base_url; ?>/login/prosesLogin" method="post">
- <div class="input-group mb-3">
-<input type="text" class="form-control" placeholder="ketikkan username.." 
-name="username">
- <div class="input-group-append">
- <div class="input-group-text">
- <span class="fas fa-user"></span>
- </div>
- </div>
- </div>
- <div class="input-group mb-3">
- <input type="password" class="form-control" placeholder="ketikkan password.." 
-name="password">
- <div class="input-group-append">
- <div class="input-group-text">
- <span class="fas fa-lock"></span>
- </div>
- </div>
- </div>
- <div class="row">
- <!-- /.col -->
- <div class="col-4">
- <button type="submit" class="btn btn-primary btn-block">Sign In</button>
- </div>
- <!-- /.col -->
- </div>
- </form>
- </div>
- <!-- /.login-card-body -->
- </div>
- <div class="row">
- <div class="col-sm-12">
- <?php
- Flasher::Message();
- ?>
- </div>
- </div>
-</div>
-<!-- /.login-box -->
-<!-- jQuery -->
-<script src="<?= base_url; ?>/plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="<?= base_url; ?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="<?= base_url; ?>/dist/js/adminlte.min.js"></script>
+    <div class="login-box">
+        <div class="login-logo">
+        <b>Buku</b>Kita
+        </div>
+        <!-- /.login-logo -->
+        <div class="card">
+            <div class="card-body login-card-body">
+                <p class="login-box-msg">Silahkan login terlebih dahulu.</p>
+                <form action="<?= base_url; ?>/login/prosesLogin" method="post">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="ketikkan username.." name="username">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-user"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="input-group mb-3">
+                    <input type="password" class="form-control" placeholder="ketikkan password.." name="password">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-lock"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                <!-- /.col -->
+                    <div class="col-4">
+                        <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                    </div>
+                <!-- /.col -->
+                </div>
+                </form>
+            </div>
+        <!-- /.login-card-body -->
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+            <?php
+            Flasher::Message();
+            ?>
+            </div>
+        </div>
+    </div>
+    <!-- /.login-box -->
+    <!-- jQuery -->
+    <script src="<?= base_url; ?>/plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="<?= base_url; ?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="<?= base_url; ?>/dist/js/adminlte.min.js"></script>
 </body>
 </html>
